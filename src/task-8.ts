0a6fab701b987af30520e7c9af8b8d64a6986298
Dimonik7772 <dimonik7772@gmail.com>
2026-03-11 14:41:15 +0100

    add all task

---
+++ src/task-8.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+interface Post {
+   body: string;
+   id: number;
+   title: string;
+   userId: number;
+}
+async function fetchPosts(): Promise<Post[]> {
+   const response = await axios.get<Post[]>(
+      "https://jsonplaceholder.typicode.com/posts",
+   );
+   return response.data;
+}
+console.log(fetchPosts());
+fetchPosts().then((posts) => {
+   console.log(posts[0].title);
+});
